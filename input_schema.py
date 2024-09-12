a6e6fcd6bcceae8165b2b6ac3e09e589334bc376
Rajdeep Borgohain <150957746+rbgo404@users.noreply.github.com>
2024-09-12 17:49:58 +0530

    Create input_schema.py

---
+++ input_schema.py
@@ -0,0 +1,8 @@
+INPUT_SCHEMA = {
+    "audio_url": {
+        'datatype': 'STRING',
+        'required': True,
+        'shape': [1],
+        'example': ["https://cdn-media.huggingface.co/speech_samples/sample2.flac"]
+    }
+}
